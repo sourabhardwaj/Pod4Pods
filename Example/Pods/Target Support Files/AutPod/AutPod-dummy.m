2dd29ebb95b51614c791bd93c9356df9ec324abc
Sourabh Bhardwaj <sourabh_bhardwaj@taaltech.com>
2019-09-30 12:34:49 +0530

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/AutPod/AutPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AutPod : NSObject
+@end
+@implementation PodsDummy_AutPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-AutPod_Example/Pods-AutPod_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AutPod_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AutPod_Example
+@end
